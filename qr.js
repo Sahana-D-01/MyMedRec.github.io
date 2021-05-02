3ba97cea3eb1db49b4093c67296095dd436e3bb4
sxfiy-a <70191393+sxfiy-a@users.noreply.github.com>
2021-05-02 12:32:19 +0400

    Uploaded files from Repl.it

---
+++ qr.js
@@ -0,0 +1,11 @@
+var image = null;
+
+function upload() {
+  //Get input from file input
+  var fileinput = document.getElementById("finput");
+  //Make new SimpleImage from file input
+  image = new SimpleImage(fileinput);
+  //Get canvas
+  var canvas = document.getElementById("can");
+  image.drawTo(canvas);
+}
